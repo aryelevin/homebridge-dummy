c9481ed7cf4f43dce9fe291177657d8823f6b597
Arye Levin <arye.l@freetail.com>
2020-12-07 01:27:59 +0200

    My version

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 "use strict";
 
-var Service, Characteristic, HomebridgeAPI;
+var Service, Characteristic;
+var fs = require('fs');
 
 module.exports = function(homebridge) {
-
+ 
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  HomebridgeAPI = homebridge;
+  
   homebridge.registerAccessory("homebridge-dummy", "DummySwitch", DummySwitch);
 }
 
 function DummySwitch(log, config) {
-  this.log = log;
-  this.name = config.name;
-  this.stateful = config.stateful;
-  this.reverse = config.reverse;
-  this.time = config.time ? config.time : 1000;		
-  this._service = new Service.Switch(this.name);
+	this.log = log;
+	this.name = config.name;
+	this.onState = undefined;
+	this.autoOffTimer = config.autoOffTimer;
   
-  this.cacheDirectory = HomebridgeAPI.user.persistPath();
-  this.storage = require('node-persist');
-  this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
-  
-  this._service.getCharacteristic(Characteristic.On)
-    .on('set', this._setOn.bind(this));
-
-  if (this.reverse) this._service.setCharacteristic(Characteristic.On, true);
-
-  if (this.stateful) {
-	var cachedState = this.storage.getItemSync(this.name);
-	if((cachedState === undefined) || (cachedState === false)) {
-		this._service.setCharacteristic(Characteristic.On, false);
-	} else {
-		this._service.setCharacteristic(Characteristic.On, true);
+	try {
+		let data = fs.readFileSync(__dirname + "/last_device_state_" + this.name, 'utf8');
+		this.onState = JSON.parse(data)['on'];
+		this.log("The devices file was loaded!");
+	} catch (err) {
+		// Here you get the error when the file was not found,
+		// but you also get any other error
+		if (err.code === 'ENOENT') {
+			this.log('File not found!');
+		} else {
+			this.log(err);
+			// throw err;
+		}
+		this.onState = false;
 	}
-  }
+	
+	this._service = new Service.Switch(this.name);
+	this._service.getCharacteristic(Characteristic.On)
+	.on('set', this._setOn.bind(this));
+	
+	this._service.setCharacteristic(Characteristic.On, this.onState);
 }
 
 DummySwitch.prototype.getServices = function() {
@@ -42,22 +45,24 @@ DummySwitch.prototype.getServices = function() {
 }
 
 DummySwitch.prototype._setOn = function(on, callback) {
-
-  this.log("Setting switch to " + on);
-
-  if (on && !this.reverse && !this.stateful) {
-    setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, false);
-    }.bind(this), this.time);
-  } else if (!on && this.reverse && !this.stateful) {
-    setTimeout(function() {
-      this._service.setCharacteristic(Characteristic.On, true);
-    }.bind(this), this.time);
-  }
+	
+	let that = this;
+	that.log("Setting switch to " + on);
+	that.onState = on;
   
-  if (this.stateful) {
-	this.storage.setItemSync(this.name, on);
-  }
+	if (on && that.autoOffTimer && that.autoOffTimer > 0) {
+		setTimeout(function() {
+			that._service.setCharacteristic(Characteristic.On, false);
+		}.bind(that), that.autoOffTimer);
+	}
   
-  callback();
+	fs.writeFile(__dirname + "/last_device_state_" + that.name, JSON.stringify({'on': that.onState}), 'utf8', function(err) {
+		if (err) {
+			that.log(err);
+		} else {
+			that.log("The device file was saved!");
+		}
+	});
+  
+	callback();
 }
